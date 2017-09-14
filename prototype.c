3eaacae23c697bc0e6461e3cff8b9e3e9d90259c
HaiAnhTrinh <31613318+HaiAnhTrinh@users.noreply.github.com>
2017-09-14 19:11:21 +1000

    Update prototype.c
    Some minor changes were made

--- prototype.c
+++ prototype.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int main(void){
-	
-	return 0;
-}
-
 struct File
 {
 	char* name;
@@ -12,8 +7,8 @@ struct File
 	double size;
 	date_t date;
 	file_t* nextp;
-
 };
+typedef struct File File_t;
 
 struct date
 {
@@ -21,13 +16,16 @@ struct date
 	int month;
 	int year;
 };
+typedef struct date date_t;
 
 struct User
 {	
-	username;
-	password;
+	char* username;
+	char* password;
 	char* status;
 };
+typedef struct User User_t;
+
 //add different levels of users i.e. admin, user guest
 //admin has access to everything, 
 //user only access to their files, guest can use it as a regular file manager
@@ -40,20 +38,20 @@ void removeFromStack(Item item);
 //user login
 void showLoginMenu();
 int login(char* username, char* password);
-void getUsername();
-void getPassword();
-void setPassword(char* password);
-void setUsername(char* username);
+void getUsername(User_t user);
+void getPassword(User_t user);
+void setPassword(User_t user, char* password);
+void setUsername(User_t user, char* username);
 void createNewUser(char* username, char* password);
 
 //File information
-void getFileName();
-void getFileLocation();
+void getFileName(file_t file);
+void getFileLocation(file_t file);
 void makeDirectory();
-void getFileSize();
-void getDate();
-void getFileType();
-void setFileName();
+void getFileSize(file_t file);
+void getDate(file_t file);
+void getFileType(file_t file);
+void setFileName(file_t file);
 void deleteFile(file_t file);
 void deleteDirectory(char* name);
 
@@ -72,3 +70,9 @@ void decrypt(file_t file);
 //compression & decompression
 void compress(file_t file);
 void decompress(file_t file);
+
+
+int main(void){
+	
+	return 0;
+}
