64c6a7f9ae5e08f661b47005c15517ad582ffde9
HaiAnhTrinh <31613318+HaiAnhTrinh@users.noreply.github.com>
2017-10-11 17:43:33 +1100

    Update project_james.c

--- project/project_james.c
+++ project/project_james.c
@@ -1304,8 +1304,6 @@ void modifyFileDetails(User_t* currentUserp, File_t* fileHeadp)
 		{
 			File_t * filep;
 			char filename[MAX_FILENAME_SIZE];
-			char compressName[MAX_FILENAME_SIZE];
-			char type[MAX_FILETYPE_SIZE];
 			int check;
 			node_t *treep;
 			int binaryTable1[27], binaryTable2[27];    
@@ -1314,8 +1312,6 @@ void modifyFileDetails(User_t* currentUserp, File_t* fileHeadp)
 			fill(binaryTable1, treep, 0);
 			invertCode(binaryTable1,binaryTable2);
 			
-			strcpy(compressName, "compressed.txt");
-			strcpy(type, "compressed");
 			printf("Please enter the name of the file you want to compress: ");
 			scanf("%s", filename);
 			
@@ -1325,7 +1321,6 @@ void modifyFileDetails(User_t* currentUserp, File_t* fileHeadp)
 				printf("ERROR, FILE NOT FOUND!!!");
 			}
 			else{
-				addFile(fileHeadp, currentUserp->username, compressName, type);
 				check = compressHuffman(filename, binaryTable2);
 				
 				if(check == 1){
@@ -1342,15 +1337,11 @@ void modifyFileDetails(User_t* currentUserp, File_t* fileHeadp)
 		{
 			File_t * filep;
 			char filename[MAX_FILENAME_SIZE];
-			char decompressName[MAX_FILENAME_SIZE];
-			char type[MAX_FILETYPE_SIZE];
 			int check;
 			node_t *treep; 
 			
 			buildTree(&treep);
 			
-			strcpy(decompressName, "decompressed.txt");
-			strcpy(type, "decompressed");
 			printf("Please enter the name of the file you want to decompress: ");
 			scanf("%s", filename);
 			
@@ -1360,7 +1351,6 @@ void modifyFileDetails(User_t* currentUserp, File_t* fileHeadp)
 				printf("ERROR, FILE NOT FOUND!!!");
 			}
 			else{
-				addFile(fileHeadp, currentUserp->username, decompressName, type);
 				check = decompressHuffman(filename, treep);
 				
 				if(check == 1){
